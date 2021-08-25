9f228e6cf3f7dfc1cec05438909afbbe2279e5e1
youngzzang326 <youngzzang326@naver.com>
2021-08-25 09:46:27 +0900

    초기버전

---
+++ src/main/java/com/study/springboot/GatTestTempApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatTestTempApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatTestTempApplication.class, args);
+	}
+
+}
